e7847eea6ff5ae57c036ea8da7703ebcc685dc18
Ikaros <1263872530@qq.com>
2022-03-08 15:52:02 +0800

    ""

---
+++ Java_codes/Code_0308/src/App.java
@@ -0,0 +1,10 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        String[] names = {"HIT", "SZ", "c++"};
+        String s = names[2];
+        names[2] = "Java";
+        System.out.println(s);
+
+        System.out.println("Hello, World!");
+    }
+}
